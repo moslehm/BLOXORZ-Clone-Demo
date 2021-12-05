79e7f3b08cd51db55f305ca045ff762a11525513
moslehm <32352647+moslehm@users.noreply.github.com>
2021-12-05 09:03:29 -0700

    Add win game state

--- src/Board.js
+++ src/Board.js
@@ -5,7 +5,7 @@ class Board {
                      [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
                      [0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0],
                      [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0],
-                     [0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0],
+                     [0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 1, 0],
                      [0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0],
                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0],
                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
@@ -14,7 +14,8 @@ class Board {
         // board[posY][posX] = 2;
         // 0 = sideways, 1 = along x, 2 = along y
         this.sideways = 0;
-        this.playerLost = false;
+        // 0 = game in progress, 1 = player lost, 2 = player won
+        this.state = 0;
     }
 
     movePlayer(direction) {
@@ -26,67 +27,79 @@ class Board {
         let x2 = this.pos2[1];
         switch (direction) {
             case "left":
-                if (sideways === 0 && board[y1][x1 - 1] === 1 && board[y1][x1 - 2] === 1) {
+                if (sideways === 0 && board[y1][x1 - 1] >= 1 && board[y1][x1 - 2] >= 1) {
                     this.sideways = 1;
                     this.pos1 = [y1, x1 - 2];
                     this.pos2 = [y1, x1 - 1];
-                } else if (sideways === 2 && board[y1][x1 - 1] === 1 && board[y2][x2 - 1] === 1) {
+                } else if (sideways === 2 && board[y1][x1 - 1] >= 1 && board[y2][x2 - 1] >= 1) {
                     this.pos1 = [y1, x1 - 1];
                     this.pos2 = [y2, x2 - 1];
-                } else if (sideways === 1 && board[y1][x1 - 1] === 1) {
+                } else if (sideways === 1 && board[y1][x1 - 1] >= 1) {
                     this.sideways = 0;
                     this.pos1 = [y1, x1 - 1];
                     this.pos2 = [undefined, undefined];
+                    if (board[this.pos1[0]][this.pos1[1]] === 2){
+                        this.state = 2;
+                    }
                 } else {
-                    this.playerLost = true;
+                    this.state = 1;
                 }
                 break;
             case "right":
-                if (sideways === 0 && board[y1][x1 + 1] === 1 && board[y1][x1 + 2] === 1) {
+                if (sideways === 0 && board[y1][x1 + 1] >= 1 && board[y1][x1 + 2] >= 1) {
                     this.sideways = 1;
                     this.pos1 = [y1, x1 + 1];
                     this.pos2 = [y1, x1 + 2];
-                } else if (sideways === 2 && board[y1][x1 + 1] === 1 && board[y2][x2 + 1] === 1) {
+                } else if (sideways === 2 && board[y1][x1 + 1] >= 1 && board[y2][x2 + 1] >= 1) {
                     this.pos1 = [y1, x1 + 1];
                     this.pos2 = [y2, x2 + 1];
-                } else if (sideways === 1 && board[y2][x2 + 1] === 1) {
+                } else if (sideways === 1 && board[y2][x2 + 1] >= 1) {
                     this.sideways = 0;
                     this.pos1 = [y2, x2 + 1];
                     this.pos2 = [undefined, undefined];
+                    if (board[this.pos1[0]][this.pos1[1]] === 2){
+                        this.state = 2;
+                    }
                 } else {
-                    this.playerLost = true;
+                    this.state = 1;
                 }
                 break;
             case "up":
-                if (sideways === 0 && board[y1 - 1][x1] === 1 && board[y1 - 2][x1] === 1) {
+                if (sideways === 0 && board[y1 - 1][x1] >= 1 && board[y1 - 2][x1] >= 1) {
                     this.sideways = 2;
                     this.pos1 = [y1 - 2, x1];
                     this.pos2 = [y1 - 1, x1];
-                } else if (sideways === 1 && board[y1 - 1][x1] === 1 && board[y2 - 1][x2] === 1) {
+                } else if (sideways === 1 && board[y1 - 1][x1] >= 1 && board[y2 - 1][x2] >= 1) {
                     this.pos1 = [y1 - 1, x1];
                     this.pos2 = [y2 - 1, x2];
-                } else if (sideways === 2 && board[y1 - 1][x1] === 1) {
+                } else if (sideways === 2 && board[y1 - 1][x1] >= 1) {
                     this.sideways = 0;
                     this.pos1 = [y1 - 1, x1];
                     this.pos2 = [undefined, undefined];
+                    if (board[this.pos1[0]][this.pos1[1]] === 2){
+                        this.state = 2;
+                    }
                 } else {
-                    this.playerLost = true;
+                    this.state = 1;
                 }
                 break;
             case "down":
-                if (sideways === 0 && board[y1 + 1][x1] === 1 && board[y1 + 2][x1] === 1) {
+                if (sideways === 0 && board[y1 + 1][x1] >= 1 && board[y1 + 2][x1] >= 1) {
                     this.sideways = 2;
                     this.pos1 = [y1 + 1, x1];
                     this.pos2 = [y1 + 2, x1];
-                } else if (sideways === 1 && board[y1 + 1][x1] === 1 && board[y2 + 1][x2] === 1) {
+                } else if (sideways === 1 && board[y1 + 1][x1] >= 1 && board[y2 + 1][x2] >= 1) {
                     this.pos1 = [y1 + 1, x1];
                     this.pos2 = [y2 + 1, x2];
-                } else if (sideways === 2 && board[y2 + 1][x2] === 1) {
+                } else if (sideways === 2 && board[y2 + 1][x2] >= 1) {
                     this.sideways = 0;
                     this.pos1 = [y2 + 1, x2];
                     this.pos2 = [undefined, undefined];
+                    if (board[this.pos1[0]][this.pos1[1]] === 2){
+                        this.state = 2;
+                    }
                 } else {
-                    this.playerLost = true;
+                    this.state = 1;
                 }
                 break;
         }
@@ -98,6 +111,6 @@ class Board {
         this.pos1 = [z * -2 + 2, x * -2 + 1];
         this.pos2 = [undefined, undefined];
         this.sideways = 0;
-        this.playerLost = 0;
+        this.state = 0;
     }
 }
